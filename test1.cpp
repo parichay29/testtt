de588ca0d5a87e0a2f7ac8b031e861dcf0c101f8
parichay_Madnani <parichayman2@gmail.com>
2019-09-01 22:56:12 +0530

    commit

--- test1.cpp
+++ test1.cpp
@@ -1,17 +1,10 @@
 #include<iostream.h>
 
-int main()
+int main() 
 {
     int a =7;
     int b=6;
-<<<<<<< HEAD
     int c=6;
-    return 0 ;
+    return 0;
 
 }
-=======
-    int d=6;
-    return 0 ;
-    
-}
->>>>>>> af51a0f2d41a033e42a413b03b8dd1ef3296a5da
